10b0567d10a3c959202f4bf166a71ebadb33e7a3
Peter Chisom <peterchisom69@gmail.com>
2022-10-12 01:30:20 +0100

    creating/- intro page & importing tmdb data 🎬🍿

---
+++ src/requests.js
@@ -0,0 +1,10 @@
+const apiKey = "c3e3b715db7ed78cea000e0969eda5d5";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language-en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language-en-US&page=1`,
+  requestPopulaTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language-en-US&page=1`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language-en-US&page=1`,
+  requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language-en-US&page=1`,
+};
+export default requests;
