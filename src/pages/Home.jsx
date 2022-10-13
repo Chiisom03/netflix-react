7e6faedfbfbb8cc51c430be00080f05605c58c8b
Peter Chisom <peterchisom69@gmail.com>
2022-10-13 02:32:15 +0100

    added slider functionalities

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <div>
       <Main />
-      <Row title="Up Coming" fetchURL={requests.requestUpComing} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
-      <Row title="Popular Trending" fetchURL={requests.requestPopulaTrending} />
-      <Row title="Horror" fetchURL={requests.requestHorror} />
-      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID='1' title="Up Coming" fetchURL={requests.requestUpComing} />
+      <Row rowID='2' title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID='3' title="Popular Trending" fetchURL={requests.requestPopulaTrending} />
+      <Row rowID='4' title="Horror" fetchURL={requests.requestHorror} />
+      <Row rowID='5' title="Top Rated" fetchURL={requests.requestTopRated} />
     </div>
   );
 };
